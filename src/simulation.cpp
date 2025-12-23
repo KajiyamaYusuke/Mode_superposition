6713ef0cb71e334913841872cacab02253296a3b
KajiyamaYusuke <y.kajiyama@biomech.me.es.osaka-u.ac.jp>
2025-12-23 18:42:34 +0900

    12/23

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -12,13 +12,13 @@ void Simulation::initialize() {
 
     params.loadFromFile("../input/param.txt", err );
 
-    geom.loadFromVTK("../input/M5/M5_mode_kawahara_mesh7_soft.vtu");
-    //geom.loadFromVTK("../input/M5/M5_mode_T2.vtu");
+    //geom.loadFromVTK("../input/M5/M5_mode_T4.vtu");
+    geom.loadFromVTK("../input/M5/M5_mode_T6_d2_soft.vtu");
     //geom.loadFromVTK("../input/old/no_mem_mode.vtu");
 
     //geom.surfExtractFromNAS("/home/kajiyama/code/simulation/input/surface_data_renewal.nas",13,18);
-    geom.surfExtractFromNAS("../input/M5/M5_surface_kawahara_mesh7_soft.nas",64,69);
-    //geom.surfExtractFromNAS("../input/M5/M5_surface_T2.nas",68,70);
+    //geom.surfExtractFromNAS("../input/M5/M5_surface_T2_d2.nas",69,66);
+    geom.surfExtractFromNAS("../input/M5/M5_surface_T6_d2.nas",72,70);
     //geom.surfExtractFromNAS("/home/kajiyama/code/simulation/input/surface_data_old_c.nas",21,30);
 
     //geom.surfExtract("/home/kajiyama/code/simulation/input/old/surface.txt", 20);
@@ -32,9 +32,9 @@ void Simulation::initialize() {
 
     mdata.initialize(params.nmode, geom);
 
-    mdata.loadFromVTU("../input/M5/M5_mode_kawahara_mesh7_soft.vtu", geom);
+    mdata.loadFromVTU("../input/M5/M5_mode_T6_d2_soft.vtu", geom);
     //mdata.loadFromVTU_old("../input/old/no_mem_mode.vtu", geom);
-    mdata.loadFreqDamping("../input/M5/M5_freq_kawahara_mesh7_soft.txt");
+    mdata.loadFreqDamping("../input/M5/M5_freq_T6_d2_soft.txt");
     //mdata.loadFreqDamping("../input/old/no_mem_frequency.txt");
 
 
